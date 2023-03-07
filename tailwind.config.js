8644cd3f1318ea3abda63f0b29231c035d91390a
Barry Melton <barry.melton@gmail.com>
2023-03-07 15:44:15 -0500

    Some more sensible tailwind defaults.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  darkMode: "media",
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./templates/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
